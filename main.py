aa8084cf6e072852d4ecd75ec4bbd50b234a18a3
p32 <ostik19791108@gmail.com>
2023-05-07 14:52:34 +0300

    oih

--- main.py
+++ main.py
@@ -1,1 +1,203 @@
-oij
+# print('hi')
+#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
+
+
+# my_dict = {
+#     'Macbook': 'pro',
+#     'Tesla': 'x'
+# }
+# print(my_dict)
+
+
+
+
+
+
+# my_dict = {                             # --> kay: value
+#     'Macbook': 'pro',
+#     'Tesla': 'x'
+# }
+# print(my_dict)
+#
+#
+# print(my_dict['Macbook'])
+
+
+
+
+
+# my_dict = {
+#     'Macbook': 'pro',
+#     'Tesla': 'X'
+# }
+#
+# kay = 'Honda'
+# value = 'Cv'
+# my_dict[kay] = value
+#
+#
+# print(my_dict)
+
+
+
+
+
+
+# my_dict = {
+#     'Macbook': 'pro',
+#     'Tesla': 'X',
+#     'Tesla': 'S'
+# }
+#
+# kay = 'Honda'
+# value = 'Cv'
+# my_dict[kay] = value
+#
+#
+# print(my_dict)
+
+
+
+
+
+# thisdict = {
+#     'brand': 'ford',
+#     'electric': 'false',
+#     'year': 1964,
+#     'colors': ['red', 'white', 'blue']
+# }
+#
+# print(thisdict['colors'])
+#
+# print(thisdict.items())
+
+
+
+
+
+# thisdict = {
+#     'brand': 'ford',
+#     'electric': 'false',
+#     'year': 1964,
+#     'colors': ['red', 'white', 'blue']
+# }
+#
+# thisdict['electric'] = True
+#
+# print(thisdict['colors'])
+#
+# print(thisdict.items())
+
+
+
+
+
+
+
+# thisdict = {
+#     'brand': 'ford',
+#     'electric': 'false',
+#     'year': 1964,
+#     'colors': ['red', 'white', 'blue']
+# }
+#
+# thisdict['electric'] = True
+# thisdict.popitem()
+#
+#
+# print(thisdict.items())
+
+
+
+
+
+# thisdict = {
+#     'brand': 'ford',
+#     'electric': 'false',
+#     'year': 1964,
+#     'colors': ['red', 'white', 'blue']
+# }
+#
+# thisdict['electric'] = True
+# del thisdict['brand']
+#
+#
+# print(thisdict.items())
+
+
+
+
+
+
+# thisdict = dict(name = 'John', age = 36, country = 'Norway')
+#
+# for x in thisdict:
+#     print(thisdict[x])
+
+
+
+
+
+# thisdict = dict(name = 'John', age = 36, country = 'Norway')
+#
+# for x,y in thisdict.items():
+#     print(x,y)
+
+
+
+
+
+# thisdict = {
+#     'brand': 'ford',
+#     'electric': 'false',
+#     'year': 1964,
+#     'colors': ['red', 'white', 'blue']
+# }
+#
+# my_dict = thisdict
+# print(my_dict)
+# my_dict['new_kay'] = 'new_value'
+# print(thisdict)
+# print(my_dict)
+
+
+
+
+
+
+# myfamily = {
+#     'child1': {
+#         'name': 'Emil'
+#         'year'
+#     }
+# }
+
+
+
+
+
+
+my_dict = {'Nick': {
+    'phone': '11111111',
+    'insta': '@123'
+},
+'Anna':{
+    'phone': '2222222',
+    'insta': '@344'
+},
+'Rick': {
+    'phone': '33333',
+    'insta': '@456'
+}
+}
+
+user = my_dict['Anna']['phone']
+
+print(user)
+
+
+
+
+
+
+
